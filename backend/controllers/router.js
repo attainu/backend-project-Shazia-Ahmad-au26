0bd7b2f0bfa10bd1bc5c4d16806181548ffc305b
unknown <shaziaahmad010@gmail.com>
2022-04-28 23:27:18 +0530

    login Apis done

--- backend/controllers/router.js
+++ backend/controllers/router.js
@@ -1,7 +1,9 @@
 const Register = require('../services/signUpService');
+const LoginService = require('../services/loginService')
 
 const router = require('express').Router();
 
 router.post("/register",Register.create);
+router.post("/login",LoginService.login);
 
 module.exports = router;
